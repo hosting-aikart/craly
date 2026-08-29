090e7aac3b08fc366a925257f5a4dc7833c7426d
aryan8790 <aryan1234tekade@gmail.com>
2026-08-29 17:54:50 +0530

    style: remove arrow icon from Sign Up button in Navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -156,11 +156,7 @@ export default function Navbar() {
               {t.nav.login}
             </Link>
             <Link href="/signup?role=business" className="craly-nav__btn craly-nav__btn--primary">
-              <span>Sign Up</span>
-              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round">
-                <line x1="5" y1="12" x2="19" y2="12" />
-                <polyline points="12 5 19 12 12 19" />
-              </svg>
+              Sign Up
             </Link>
           </div>
 
